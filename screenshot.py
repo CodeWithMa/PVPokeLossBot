5725cad98a147479cca024b98a50473a21ba93da
Eswar Balasubramanian <eswarbtechit@gmail.com>
2024-01-21 19:09:29 +0530

    Windows environment fixes

--- screenshot.py
+++ screenshot.py
@@ -6,6 +6,6 @@ def capture_screenshot(filename):
     Captures a screenshot of the Android screen using adb and saves it to a file.
     Returns True if the adb command was successful, False otherwise.
     """
-    adb_command = "adb exec-out screencap -p > {} 2> /dev/null".format(filename)
+    adb_command = "adb exec-out screencap -p > {}".format(filename)
     error_code = os.system(adb_command)
     return error_code == 0
