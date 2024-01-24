105967108cd826b32ae37a5a216d6b90c09bc3b9
Ma <101021254+CodeWithMa@users.noreply.github.com>
2024-01-24 17:44:07 +0100

    Check platform.system() to use different adb command for windows

--- screenshot.py
+++ screenshot.py
@@ -1,4 +1,5 @@
 import os
+import platform
 
 
 def capture_screenshot(filename):
@@ -6,6 +7,9 @@ def capture_screenshot(filename):
     Captures a screenshot of the Android screen using adb and saves it to a file.
     Returns True if the adb command was successful, False otherwise.
     """
-    adb_command = "adb exec-out screencap -p > {}".format(filename)
+    if platform.system() == "Windows":
+        adb_command = "adb exec-out screencap -p > {}".format(filename)
+    else:
+        adb_command = "adb exec-out screencap -p > {} 2> /dev/null".format(filename)
     error_code = os.system(adb_command)
     return error_code == 0
