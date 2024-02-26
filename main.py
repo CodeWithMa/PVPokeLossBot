34282aff76f8008af61831137772241872b376bf
Ma <101021254+CodeWithMa@users.noreply.github.com>
2024-02-26 16:27:04 +0100

    Add --verbose command line flag to set the log level to DEBUG.

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
+import argparse
 import logging
 
 from src import bot
 
 
-def set_up_logging_configuration():
+def set_up_logging_configuration(log_level):
     logging.basicConfig(
-        level=logging.INFO,
+        level=log_level,
         format="%(asctime)s %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
     )
 
+parser = argparse.ArgumentParser(description="PVPokeLossBot is a bot designed for the PVP mode of the mobile game Pokemon Go.")
+parser.add_argument('--verbose', '-v', action='store_true', help='Enable verbose logging')
 
-set_up_logging_configuration()
+args = parser.parse_args()
+
+set_up_logging_configuration(logging.DEBUG if args.verbose else logging.INFO)
 
 try:
     bot.run()
